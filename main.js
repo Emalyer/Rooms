f55ca191d0066447e7e41ad1c0814d970ae51f48
Emalyer <77217260+Emalyer@users.noreply.github.com>
2021-11-03 16:48:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+function living_room(){
+    window.location="Livingroom.html";
+}
+function kitchen(){
+    window.location="Kitchen.html";
+}
+function study_room(){
+    window.location="studyroom.html";
+}
+function playroom(){
+    window.location="playroom.html";
+}
